ea30270074dd74c29cbdfd098cd6eac6c74dade5
Matthew Schmitt <matthew.schmitt@riskiq.net>
2016-06-24 15:40:05 -0700

    Switching class back to using puppet/archive module

--- manifests/init.pp
+++ manifests/init.pp
@@ -118,24 +118,18 @@ class kafka (
     ],
   }
 
-  #include '::archive'
+  include '::archive'
 
   archive { "${package_dir}/${basefilename}":
-    name            => $package_name,
     ensure          => present,
-    extension       => 'tgz',
-    #extract         => true,
-    #extract_command => 'tar xfz %s --strip-components=1',
-    #extract_path    => $install_directory,
-    target          => $install_directory,
-    #source          => $package_url,
-    src_target      => '/var/tmp',
-    url             => $package_url,
-    #creates         => "${install_directory}/config",
-    #cleanup         => true,
+    extract         => true,
+    extract_command => 'tar xfz %s --strip-components=1',
+    extract_path    => $install_directory,
+    source          => $package_url,
+    creates         => "${install_directory}/config",
+    cleanup         => true,
     user            => 'kafka',
-    #group           => 'kafka',
-    digest_type     => 'sha2',
+    group           => 'kafka',
     require         => [
       File[$package_dir],
       File[$install_directory],
